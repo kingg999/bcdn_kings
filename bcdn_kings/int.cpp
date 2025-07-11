1d5877c30695db72f8449447ce0ab1ff1d704f9f
king999 <syahidirahman009@gmail.com>
2025-07-11 10:42:42 +0800

    Latihan 1 jerr

---
+++ bcdn_kings/int.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+      int angka;
+
+      cout << "pilih angka yang kamu mau =";
+      cin >> angka;
+
+      cout << "angka yang kamu pilih adalah =" << angka;
+}
