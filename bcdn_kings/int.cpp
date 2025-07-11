6cceedc5453e87718bf710ca8f658047b5c104d2
king999 <syahidirahman009@gmail.com>
2025-07-11 10:47:41 +0800

    Laithan 1 jerrr

--- bcdn_kings/int.cpp
+++ bcdn_kings/int.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
       int angka;
 
-      cout << "pilih angka yang kamu mau =";
+      cout << "pilih angka yang kamu mau = ";
       cin >> angka;
 
-      cout << "angka yang kamu pilih adalah =" << angka;
+      cout << "angka yang kamu pilih adalah = " << angka;
 }
---
+++ bcdn_kings/string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+      string nama;
+
+      cout << "ketik nama anda = ";
+      cin >> nama;
+
+      cout << "nama anda adalah = " << nama;
+}
